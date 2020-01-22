47f19713f67a66f6680fa9a157ec9ee5064da427
Anthony Pecchillo <anthonypecchillo@gmail.com>
2020-01-22 19:33:38 +0100

    Add Description, ForestMonitoringAndMeasurementSystems, Demographics, and Economics tiles to NJOverviewPage

--- src/components/NJOverviewPage.jsx
+++ src/components/NJOverviewPage.jsx
@@ -6,42 +6,63 @@ import React from 'react';
 import styled from 'styled-components';
 
 import NJContacts from './NJContacts';
+import NJDescription from './NJDescription';
+import NJForestMonitoringAndMeasurementSystems from './NJForestMonitoringAndMeasurementSystems';
+import NJDemographics from './NJDemographics';
+import NJEconomics from './NJEconomics';
 import Tile from './Tile';
 
 const OverviewGrid = styled.div`
   display: grid;
-  grid-gap: 2%;
+  grid-column-gap: 2%;
+  grid-row-gap: 25px;
   grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-template-rows: 800px 800px 1000px 400px;
+  grid-template-rows: 800px 800px 1000px 400px 550px;
   grid-template-areas:
-    '. . . .'
-    '. . . .'
-    '. . . .'
-    'contacts contacts contacts contacts';
+    'description description description description'
+    'fmms fmms demographics demographics'
+    'economics economics . .'
+    'contacts contacts contacts contacts'
+    '. . . .';
 
   height: 100%;
 
   @media (max-width: 765px) {
     grid-template-areas:
-      '. . . .'
-      '. . . .'
-      'contacts contacts . .'
-      '. . . .';
+      'description description description description'
+      'fmms fmms fmms fmms'
+      'contacts contacts demographics demographics'
+      'economics economics . .'
+      'economics economics . .';
   }
 
   @media (max-width: 460px) {
     grid-template-areas:
-      '. . . .'
-      '. . . .'
+      'description description description description'
+      'fmms fmms fmms fmms'
+      'demographics demographics demographics demographics'
       'contacts contacts contacts contacts'
-      '. . . .';
+      'economics economics economics economics';
+    grid-template-rows: 800px 800px 800px 1000px 1000px;
   }
 `;
 
-const NJOverviewPage = ({ nationName, jurisdictionName }) => (
+const NJOverviewPage = ({ nationName, jurisdictionName, language }) => (
   <OverviewGrid>
+    <Tile gridArea="description">
+      <NJDescription jurisdiction={jurisdictionName} language={language} />
+    </Tile>
+    <Tile gridArea="fmms">
+      <NJForestMonitoringAndMeasurementSystems jurisdiction={jurisdictionName} language={language} />
+    </Tile>
+    <Tile gridArea="demographics">
+      <NJDemographics jurisdiction={jurisdictionName} language={language} />
+    </Tile>
     <Tile gridArea="contacts">
       <NJContacts jurisdiction={jurisdictionName} />
+    </Tile>
+    <Tile gridArea="economics">
+      <NJEconomics jurisdiction={jurisdictionName} language={language} />
     </Tile>
   </OverviewGrid>
 );
